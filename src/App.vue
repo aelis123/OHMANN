f0bc6e684ecee11690b055d852a077d006e60d6f
aelis123 <gehring.alice@web.de>
2024-07-02 10:15:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div id="layout">
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Events</RouterLink> |
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+
+  <RouterView />
+  </div>
+</template>
+
+<style>
+#layout {
+font-family: Avenir, Arial, Helvetica, sans-serif;
+-webkit-font-smoothing: antialiased;
+
+text-align: center;
+color: #2c3e50;  
+}
+
+nav {
+  padding: 30px;
+} 
+
+nav a {
+  font-weight: bold;  
+  color: lightgrey;
+}
+
+nav a.router-link-exact-active {
+  color: #2c3e50;
+}
+
+h2 {
+  font-size: 20px;
+}
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import EventCard from '@/components/EventCard.vue'
+import { ref, onMounted } from 'vue';
+import EventService from '@/services/EventService';
+
+const events = ref(null)
+
+onMounted (()=> {
+  EventService  .getEvents()
+  .then((response) => {
+    events.value = response.data
+
+    console.log('events:', response.data)
+  })
+
+  .catch((error) => {
+    console.log(error)
+  }) 
+  })
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+ <div class="events"> 
+  <EventCard v-for="event in events" :key="event.id" :event="event"/>
+ </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
+</style>
